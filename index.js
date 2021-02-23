87867891dbd485fa2a27960d81258bb103d38ea7
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2021-02-23 20:42:27 +0300

    v1.0.0

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express=require('express');
 const logger=require('morgan');
 const path=require('path');
 const fileUpload=require('express-fileupload');
+const flash=require('express-flash');
+const session=require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
 
 // Initiate Express Application
 const app=express();
@@ -16,8 +19,33 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(logger('combined'));
 app.use(fileUpload());
+app.use(express.json());
 
 const Index=require('./routes/file');
+
+// Express Sessions
+const options = {
+     host: process.env.DB_HOST,
+     port: process.env.DB_PORT,
+     user: process.env.DB_USER,
+     password: process.env.DB_PASSWORD,
+     database: process.env.DB
+};
+const sessionStore = new MySQLStore(options);
+
+// express-session
+app.use(session({
+     secret: process.env.SESSION_KEY,
+     store: sessionStore,
+     resave: false,
+     saveUninitialized: false,
+     // cookie: {
+     //      maxAge: 1000*60*60*24,
+     //      secure:true,
+     // }
+}));
+// Express Flash
+app.use(flash());
 
 app.use('/file',Index);
 
