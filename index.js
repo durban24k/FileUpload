7c6c211de8e41e9a15896bf012bdc950b27ea8b3
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2021-02-23 18:44:32 +0300

    v1.0.0

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ require('dotenv').config({path:'./.env'});
 const express=require('express');
 const logger=require('morgan');
 const file_upload=require('express-fileupload');
+
+// Initiate Express Application
+const app=express();
