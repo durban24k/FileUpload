16a4fe7bcc46c1ad60222dc5c2da8c1400ab7157
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2021-02-23 23:54:07 +0300

    v1.0.0

--- routes/file.js
+++ routes/file.js
@@ -8,7 +8,43 @@ Router.get('/',(req,res)=>{
 
 Router.post('/',(req,res)=>{
      console.log(req.body);
-     console.log(req.files.profile_image);
+     console.log(req.files);
+
+     const {
+          name
+     }=req.body;
+     if(!req.files){
+          req.flash('alert_msg','No Profile Picture Was Added!!');
+          res.redirect('/file')
+     }else{
+          const dp=req.files.profile_image;
+          const image_name=dp.name;
+
+          if(dp.mimetype=="image/jpeg" || dp.mimetype=="image/png"){
+               dp.mv(`./public/images/profile/${image_name}`,(err)=>{
+                    if (err) return res.status(500).send(err);
+                    const db=require('../config/db');
+                    const sql="INSERT INTO images SET ?";
+                    const timestamp=Date.now();
+                    db.query(sql,{name,image_url:image_name,created_at:timestamp,updated_at:timestamp},(error,results,fields)=>{
+                         if(error) throw error;
+                         req.flash('success_msg','Image Added Successfully!');
+                         res.redirect('/file');
+                    });
+               });
+          }else{
+               req.flash('alert_msg','This format is not allowed , please upload file with ".png",".gif",".jpg"');
+               res.redirect('/file');
+          }
+     }
+});
+
+Router.get('/profile',(req,res)=>{
+     res.render('profile');
+});
+
+Router.post('/profile',(req,res)=>{
+     console.log(req.body);zoom
 });
 
 module.exports = Router;
