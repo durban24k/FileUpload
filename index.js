2357ec9142b8fc62c17bca1c274bcba0a2c7e40e
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2021-02-23 19:14:49 +0300

    v1.0.0

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 require('dotenv').config({path:'./.env'});
 const express=require('express');
 const logger=require('morgan');
+const path=require('path');
 const file_upload=require('express-fileupload');
 
 // Initiate Express Application
 const app=express();
+
+// Setting the View Engine
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','hbs');
+
+// MiddleWare
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(logger('combined'));
+
+// Port Listening
+const port=process.env.PORT || 3000;
+
+app.listen(port,()=>{
+     console.log(`Server Listening on port ${port}`);
+});
